d89b7c9b3574f9a36a7783a67d88b15e7cb528ba
Vas Andrus <vasyland@gmail.com>
2018-10-29 17:52:53 -0400

    Added q42 from IBM Tests

--- InterviewTasks/src/lambdas/BasicsFromYoutube.java
+++ InterviewTasks/src/lambdas/BasicsFromYoutube.java
@@ -1,17 +1,39 @@
 package lambdas;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 /**
  * https://www.youtube.com/watch?v=t1-YZ6bF-g0
  */
 public class BasicsFromYoutube {
 
 	public static void main(String[] args) {
+		
+		BasicsFromYoutube bfy = new BasicsFromYoutube();
+		
+//      IntStream.range(1,8).skip(2).forEach(System.out::println);
+//      System.out.println(IntStream.range(1,5).sum());
+		
+      Stream.of("WABC","Telus","Capgemini").sorted().findFirst().ifPresent(System.out::println);
+      
+//      Stream.of(263,49,59,23).sorted().findFirst().ifPresent(System.out::println);
 
 		String[] deps = { "Telus", "Capgemini", "Mackenzie Investments" };
 		Arrays.stream(deps).sorted().forEach(System.out::println);
 
+		
+		bfy.ListSkipQ42();
+		
 	}
-
+	
+	public void ListSkipQ42() {
+        List<Integer> numbers=Arrays.asList(1,2,3,4,5);
+        List<Integer> result = numbers.stream().skip(2)
+            .collect(Collectors.toCollection(ArrayList::new));
+        System.out.println(result.toString());
+	}
 }
